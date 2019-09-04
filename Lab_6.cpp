5a1f0ac514e41cb7796bcb9c32751f165d30a3fa
anjgodara <53431908+anjgodara@users.noreply.github.com>
2019-09-04 06:55:45 +0530

    Add files via upload

--- Lab_6.cpp
+++ Lab_6.cpp
@@ -1,37 +1,67 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+typedef long long int ll;
+long long find(vector<long long> v,long long k)
+{
+    long long start=0,end=v.size()-1,mid;
+    while(start<=end)
+    {
+        mid=(start+end)/2;
+        if(v[mid]>k)
+            end=mid-1;
+        else if(v[mid]<k)
+            start=mid+1;
+        else return mid;
+    }
+    return -1;
+}
 int main()
 {
-  ll t;
-  cin>>t;
-  while(t--)
-  {
-   ll n;
-   cin>>n;
-   vector<ll>v{0};
-   pair<ll,ll>p;
-   p = {-1,-1};
-   while(n--)
-   {
-    ll a;
-    cin>>a;
-    if(a==1)
+    ll n,q,maxid=-1;
+    cin>>n>>q;
+
+    ll x,y,z;
+    vector<vector<ll>> v;
+    vector<long long> w;
+    for(int i=0;i<q;i++)
     {
-        ll amt,id;
-        cin>>id>>amt;
-        v[id]+=amt;
-        if(v[id]>p.second)
+        cin>>x;
+        if(x==1)
         {
-            p = {id,v[id]};
+            cin>>y>>z;
+            w.push_back(y);
+            v.push_back({x,y,z});
+        }
+        else v.push_back({2});
+    }
+    sort(w.begin(),w.end());
+    vector<ll> u;
+    for(int i=0;i<w.size();i++)
+    {
+        if(!i)
+            u.push_back(w[i]);
+        if(w[i]!=w[i-1])
+            u.push_back(w[i]);
+    }
+    ll am[u.size()]={0};
+    for(int i=0;i<q;i++)
+    {
+        if(v[i][0]==1)
+        {
+            long long p=find(u,v[i][1]);
+            am[p]+=v[i][2];
+            //cout<<u[p]<<' '<<am[p]<<endl;
+            if(maxid==-1||am[p]>am[maxid])
+            {
+                maxid=p;
+                //cout<<i<<'o'<<endl;
+            }
+        }
+        else
+        {
+            if(maxid==-1)
+                cout<<"No data available."<<endl;
+            else cout<<u[maxid]<<endl;
         }
     }
-    else if(a==2)
-    {
-        if(p.first==-1)
-            cout<<"No data available"<<endl;
-        else
-            cout<<p.first<<endl;
-    }
-   }}
 }
