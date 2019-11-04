76a2141b933ae46f8f4fc673a7bbd6243253c402
anjgodara <53431908+anjgodara@users.noreply.github.com>
2019-11-04 13:14:55 +0530

    Update Binary_search_tree.cpp

--- Lab_Slot_1/Binary_search_tree.cpp
+++ Lab_Slot_1/Binary_search_tree.cpp
@@ -40,25 +40,14 @@ struct node * minValueNode(struct node* node)
     return current; 
 } 
 struct node* deleteNode(struct node* root, int key) 
-{ 
-    // base case 
+{
     if (root == NULL) return root; 
-  
-    // If the key to be deleted is smaller than the root's key, 
-    // then it lies in left subtree 
     if (key < root->key) 
         root->left = deleteNode(root->left, key); 
-  
-    // If the key to be deleted is greater than the root's key, 
-    // then it lies in right subtree 
     else if (key > root->key) 
         root->right = deleteNode(root->right, key); 
-  
-    // if key is same as root's key, then This is the node 
-    // to be deleted 
     else
     { 
-        // node with only one child or no child 
         if (root->left == NULL) 
         { 
             struct node *temp = root->right; 
@@ -71,27 +60,14 @@ struct node* deleteNode(struct node* root, int key)
             free(root); 
             return temp; 
         } 
-  
-        // node with two children: Get the inorder successor (smallest 
-        // in the right subtree) 
         struct node* temp = minValueNode(root->right); 
-  
-        // Copy the inorder successor's content to this node 
         root->key = temp->key; 
-  
-        // Delete the inorder successor 
         root->right = deleteNode(root->right, temp->key); 
     } 
     return root; 
 } 
 int main() 
 { 
-    /* Let us create following BST 
-              50 
-           /     \ 
-          30      70 
-         /  \    /  \ 
-       20   40  60   80 */
     struct node *root = NULL; 
     root = insert(root, 50); 
     root = insert(root, 30); 
@@ -103,8 +79,6 @@ int main()
   
     printf("Inorder traversal of the given tree \n"); 
     inorder(root);
-    //struct node* temp = minValueNode(root); 
-   // cout<<temp->key<<endl;
     printf("\nDelete 20\n"); 
     root = deleteNode(root, 20); 
     printf("Inorder traversal of the modified tree \n"); 
